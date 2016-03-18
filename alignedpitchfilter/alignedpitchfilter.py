4b12599c7bb8a9853a5c7ed7f47e392146a5d6fa
Sertan Senturk <contact@sertansenturk.com>
2016-03-18 02:25:31 +0100

    Refactored AlignedPitchFilter to a class

--- alignedpitchfilter/alignedpitchfilter.py
+++ alignedpitchfilter/alignedpitchfilter.py
@@ -2,151 +2,154 @@ import numpy as np
 import copy
 
 
-def corect_octave_errors(pitch, notes):
-    # IMPORTANT: In the audio-score alignment step, the pitch value
-    # of each note is computed from the theoretical pitch distance
-    # from the tonic and de-normalized according to the tonic
-    # frequency of the performance. The value is not computed from
-    # THE PITCH TRAJECTORY OF THE NOTE; it is just the THEORETICAL
-    # FREQUENCY OF THE NOTE SYMBOL ACCORDING TO THE TONIC FREQUENY
-    # The performed stable pitch of the note will be computed in the
-    # aligned-note-models
+class AlignedPitchFilter(object):
+    @classmethod
+    def corect_octave_errors(cls, pitch, notes):
+        # IMPORTANT: In the audio-score alignment step, the pitch value
+        # of each note is computed from the theoretical pitch distance
+        # from the tonic and de-normalized according to the tonic
+        # frequency of the performance. The value is not computed from
+        # THE PITCH TRAJECTORY OF THE NOTE; it is just the THEORETICAL
+        # FREQUENCY OF THE NOTE SYMBOL ACCORDING TO THE TONIC FREQUENY
+        # The performed stable pitch of the note will be computed in the
+        # aligned-note-models
 
-    notes_corrected = copy.deepcopy(notes)
+        pitch_corrected = np.copy(pitch)
+        notes_corrected = copy.deepcopy(notes)
 
-    # remove skipped notes
-    notes_corrected = ([n for n in notes_corrected
-                        if not n['Interval'][0] == n['Interval'][1]])
+        # remove skipped notes
+        notes_corrected = ([n for n in notes_corrected
+                            if not n['Interval'][0] == n['Interval'][1]])
 
-    # remove rests
-    notes_corrected = [n for n in notes_corrected if
-                       n['TheoreticalPitch']['Value']]
+        # remove rests
+        notes_corrected = [n for n in notes_corrected if
+                           n['TheoreticalPitch']['Value']]
 
-    # group the notes into sections
-    synth_pitch = notes_to_synth_pitch(notes_corrected, pitch[:, 0])
+        # group the notes into sections
+        synth_pitch = cls._notes_to_synth_pitch(notes_corrected,
+                                               pitch_corrected[:, 0])
 
-    # octave correction
-    pitch_corrected = np.copy(pitch)
-    for i, sp in enumerate(synth_pitch):
-        pitch_corrected[i][1] = move_to_same_octave(pitch[i][1], sp)
+        # octave correction
+        for i, sp in enumerate(synth_pitch):
+            pitch_corrected[i][1] = cls._move_to_same_octave(
+                pitch_corrected[i][1], sp)
 
-    for nc in notes_corrected:
-        trajectory = np.vstack(p[1] for p in pitch_corrected
-                               if
-                               nc['Interval'][0] <= p[0] <= nc['Interval'][1])
-        nc['PerformedPitch']['Value'] = np.median(trajectory).tolist()
-    # import pdb
-    # pdb.set_trace()
-    return pitch_corrected, synth_pitch, notes_corrected
+        for nc in notes_corrected:
+            trajectory = np.vstack(
+                p[1] for p in pitch_corrected
+                if nc['Interval'][0] <= p[0] <= nc['Interval'][1])
+            nc['PerformedPitch']['Value'] = np.median(trajectory).tolist()
 
+        return pitch_corrected, synth_pitch, notes_corrected
 
-def notes_to_synth_pitch(notes, time_stamps, max_boundary_tol=6):
-    synth_pitch = np.array([0] * len(time_stamps))
+    @classmethod
+    def _notes_to_synth_pitch(cls, notes, time_stamps, max_boundary_tol=6):
+        synth_pitch = np.array([0] * len(time_stamps))
 
-    for i in range(0, len(notes)):
-        prevlabel = ([] if i == 0 else
-                     notes[i - 1]['Label'].split('--')[0])
-        label = notes[i]['Label'].split('--')[0]
-        nextlabel = ([] if i == len(notes) - 1 else
-                     notes[i + 1]['Label'].split('--')[0])
+        for i in range(0, len(notes)):
+            prevlabel = ([] if i == 0 else
+                         notes[i - 1]['Label'].split('--')[0])
+            label = notes[i]['Label'].split('--')[0]
+            nextlabel = ([] if i == len(notes) - 1 else
+                         notes[i + 1]['Label'].split('--')[0])
 
-        # pre interpolation start time on the first note
-        if not prevlabel:
-            startidx = find_closest_sample_idx(
-                notes[i]['Interval'][0] - max_boundary_tol, time_stamps)
-        elif not label == prevlabel:
-            # post interpolation start time on a group start
+            # pre interpolation start time on the first note
+            if not prevlabel:
+                startidx = cls._find_closest_sample_idx(
+                    notes[i]['Interval'][0] - max_boundary_tol, time_stamps)
+            elif not label == prevlabel:
+                # post interpolation start time on a group start
 
-            # recalculate the end time of the previous
-            tempstartidx = find_closest_sample_idx(
-                notes[i]['Interval'][0], time_stamps)
-            prevendidx = find_closest_sample_idx(
-                notes[i - 1]['Interval'][1] + max_boundary_tol,
-                time_stamps[:tempstartidx])
+                # recalculate the end time of the previous
+                tempstartidx = cls._find_closest_sample_idx(
+                    notes[i]['Interval'][0], time_stamps)
+                prevendidx = cls._find_closest_sample_idx(
+                    notes[i - 1]['Interval'][1] + max_boundary_tol,
+                    time_stamps[:tempstartidx])
 
-            startidx = prevendidx + find_closest_sample_idx(
-                notes[i]['Interval'][0] - max_boundary_tol,
-                time_stamps[prevendidx:]) + 1
-        else:  # no pre interpolation
-            startidx = find_closest_sample_idx(
-                notes[i]['Interval'][0], time_stamps)
+                startidx = prevendidx + cls._find_closest_sample_idx(
+                    notes[i]['Interval'][0] - max_boundary_tol,
+                    time_stamps[prevendidx:]) + 1
+            else:  # no pre interpolation
+                startidx = cls._find_closest_sample_idx(
+                    notes[i]['Interval'][0], time_stamps)
 
-        if not nextlabel:
-            # post interpolation end time on the last note
-            endidx = find_closest_sample_idx(
-                notes[i]['Interval'][1] + max_boundary_tol, time_stamps)
-        elif not label == nextlabel:
-            # post interpolation end time on a group end
-            nextstartidx = find_closest_sample_idx(
-                notes[i + 1]['Interval'][0], time_stamps)
-            endidx = find_closest_sample_idx(
-                notes[i]['Interval'][1] + max_boundary_tol,
-                time_stamps[:nextstartidx])
-        else:
-            # post interpolation within a group
-            nextstartidx = find_closest_sample_idx(
-                notes[i + 1]['Interval'][0], time_stamps)
-            endidx = nextstartidx - 1
-
-        synth_pitch[startidx:endidx + 1] = \
-            notes[i]['TheoreticalPitch']['Value']
-
-    return synth_pitch
-
-
-def find_closest_sample_idx(val, sample_vals):
-    return np.argmin(abs(sample_vals - val))
-
-
-def move_to_same_octave(pp, sp):
-    minpp = pp
-    if not (pp == 0 or sp == 0):
-        direction = 1 if sp > pp else -1
-
-        prev_cent_diff = 1000000  # assign an absurd number
-        decr = True
-        while decr:
-            cent_diff = abs(hz2cent(pp, sp))
-            if prev_cent_diff > cent_diff:
-                minpp = pp
-                pp *= 2 ** direction
-                prev_cent_diff = cent_diff
+            if not nextlabel:
+                # post interpolation end time on the last note
+                endidx = cls._find_closest_sample_idx(
+                    notes[i]['Interval'][1] + max_boundary_tol, time_stamps)
+            elif not label == nextlabel:
+                # post interpolation end time on a group end
+                nextstartidx = cls._find_closest_sample_idx(
+                    notes[i + 1]['Interval'][0], time_stamps)
+                endidx = cls._find_closest_sample_idx(
+                    notes[i]['Interval'][1] + max_boundary_tol,
+                    time_stamps[:nextstartidx])
             else:
-                decr = False
+                # post interpolation within a group
+                nextstartidx = cls._find_closest_sample_idx(
+                    notes[i + 1]['Interval'][0], time_stamps)
+                endidx = nextstartidx - 1
 
-    return minpp
+            synth_pitch[startidx:endidx + 1] = \
+                notes[i]['TheoreticalPitch']['Value']
 
+        return synth_pitch
 
-def cent2hz(cent_val, ref_hz):
-    try:
-        return ref_hz * 2 ** (cent_val / 1200.0)
-    except TypeError:  # _NaN_; rest
-        return None
+    @staticmethod
+    def _find_closest_sample_idx(val, sample_vals):
+        return np.argmin(abs(sample_vals - val))
 
+    @classmethod
+    def _move_to_same_octave(cls, pp, sp):
+        minpp = pp
+        if not (pp == 0 or sp == 0):
+            direction = 1 if sp > pp else -1
 
-def hz2cent(val, ref_hz):
-    return 1200.0 * np.log2(val / ref_hz)
+            prev_cent_diff = 1000000  # assign an absurd number
+            decr = True
+            while decr:
+                cent_diff = abs(cls._hz2cent(pp, sp))
+                if prev_cent_diff > cent_diff:
+                    minpp = pp
+                    pp *= 2 ** direction
+                    prev_cent_diff = cent_diff
+                else:
+                    decr = False
 
+        return minpp
 
-def decompose_into_chunks(pitch, bottom_limit=0.7, upper_limit=1.3):
-    pitch_chunks = []
-    temp_pitch = np.array([])
-    # starts at the first sample
-    for i in range(1, len(pitch) - 1):
-        # ignore chunks with 0 frequency values
-        if pitch[i][1] == 0:
-            pass
-        # non-zero chunks
-        else:
-            interval = float(pitch[i + 1][1]) / float(pitch[i][1])
-            temp_pitch = (np.vstack((temp_pitch, pitch[i]))
-                          if temp_pitch.size > 0 else pitch[i])
+    @staticmethod
+    def _cent2hz(cent_val, ref_hz):
+        try:
+            return ref_hz * 2 ** (cent_val / 1200.0)
+        except TypeError:  # _NaN_; rest
+            return None
 
-            if not bottom_limit < interval < upper_limit:
-                if len(temp_pitch) > 0:
-                    pitch_chunks.append(temp_pitch)
-                temp_pitch = np.array([])
-    if len(temp_pitch) > 0:
-        pitch_chunks.append(temp_pitch)
+    @staticmethod
+    def _hz2cent(val, ref_hz):
+        return 1200.0 * np.log2(val / ref_hz)
 
-    return pitch_chunks
+    @staticmethod
+    def _decompose_into_chunks(pitch, bottom_limit=0.7, upper_limit=1.3):
+        pitch_chunks = []
+        temp_pitch = np.array([])
+        # starts at the first sample
+        for i in range(1, len(pitch) - 1):
+            # ignore chunks with 0 frequency values
+            if pitch[i][1] == 0:
+                pass
+            # non-zero chunks
+            else:
+                interval = float(pitch[i + 1][1]) / float(pitch[i][1])
+                temp_pitch = (np.vstack((temp_pitch, pitch[i]))
+                              if temp_pitch.size > 0 else pitch[i])
+
+                if not bottom_limit < interval < upper_limit:
+                    if len(temp_pitch) > 0:
+                        pitch_chunks.append(temp_pitch)
+                    temp_pitch = np.array([])
+        if len(temp_pitch) > 0:
+            pitch_chunks.append(temp_pitch)
+
+        return pitch_chunks
